0c7a36b36ba47b58564cd72ca0bd030ec103615c
Kir <ionow.k@yandex.ru>
2026-01-22 18:22:40 +0300

    fix

--- docs/js/hide-ads.js
+++ docs/js/hide-ads.js
@@ -1,10 +1,32 @@
-document.addEventListener('DOMContentLoaded', function() {
+(function() {
   const removeAds = () => {
-    const ads = document.querySelectorAll('[data-ea-publisher], .ea-placement, .ea-content, .ea-callout, [id*="readthedocs-ea"]');
-    ads.forEach(ad => ad.remove());
+    const selectors = [
+      '[data-ea-publisher]',
+      '.ea-placement',
+      '.ea-content', 
+      '.ea-callout',
+      '.ea-type-text',
+      '[id*="readthedocs-ea"]',
+      '[class*="raised"][class*="adaptive"]',
+      'div[data-ea-manual]'
+    ];
+    
+    selectors.forEach(selector => {
+      document.querySelectorAll(selector).forEach(el => el.remove());
+    });
   };
-  
+
+  // Удаляем сразу и с задержкой (реклама грузится асинхронно)
   removeAds();
-  setTimeout(removeAds, 1000);
+  document.addEventListener('DOMContentLoaded', removeAds);
+  setTimeout(removeAds, 500);
+  setTimeout(removeAds, 1500);
   setTimeout(removeAds, 3000);
-});
+  
+  // Observer для динамически добавленных элементов
+  const observer = new MutationObserver(removeAds);
+  observer.observe(document.body || document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+})();
